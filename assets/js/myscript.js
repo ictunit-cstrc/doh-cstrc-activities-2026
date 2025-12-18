e1e082b4e436821ab9b08201c67bea9a9c5de728
ictunit-cstrc <ict.unit@cstrc.doh.gov.ph>
2025-12-18 13:59:19 +0800

    Add files via upload
    First Upload

---
+++ assets/js/myscript.js
@@ -0,0 +1,88 @@
+ let activities = {};
+    function groupByMonth(activities) {
+      return activities.reduce((result, item) => {
+        const date = new Date(item.DATE);
+        const month = date.toLocaleString("en-US", { month: "long" });
+
+        if (!result[month]) {
+          result[month] = [];
+        }
+
+        result[month].push({
+          ...item,
+          displayDate: date.toLocaleDateString("en-US", {
+            month: "short",
+            day: "numeric"
+          })
+        });
+
+        return result;
+      }, {});
+    }
+
+  fetch("json/activities.json")
+  .then(res => res.json())
+  .then(data => {
+     activities = groupByMonth(data);
+   renderMonths();
+  });
+  
+function renderMonths() {
+  const monthsEl = document.getElementById('months');
+  monthsEl.innerHTML = '';
+
+  Object.keys(activities).forEach(month => {
+
+    const col = document.createElement('div');
+    col.className = 'col-4';
+
+    const item = document.createElement('div');
+    item.className = 'item wow fadeIn';
+    item.onclick = () => openModal(month);
+
+    const icon = document.createElement('div');
+    icon.className = 'item-logo';
+    icon.innerHTML = '<i class="fa fa-calendar"></i>';
+
+    const rightText = document.createElement('div');
+    rightText.className = 'right-text';
+    rightText.innerHTML = `
+      <h4 style="font-size:12pt;">${month}</h4>
+      <p>${activities[month].length} Activities</p>
+    `;
+
+    item.appendChild(icon);
+    item.appendChild(rightText);
+    col.appendChild(item);
+    monthsEl.appendChild(col);
+  });
+}
+
+  function openModal(month) {
+    document.getElementById('modalTitle').textContent = month + ' 2026';
+    const body = document.getElementById('tbldata');
+    body.innerHTML = '';
+
+    activities[month].forEach(act => {
+      //body.innerHTML += `<div class="activity"><span class="date">${act.DATE}</span> – ${act.title}</div>`;
+      const date = new Date(act.DATE);
+
+      const formatted = date.toLocaleDateString("en-US", {
+        month: "short",
+        day: "numeric"
+      });
+      body.innerHTML += `<tr>
+              <td>${formatted}</td>
+              <td>${act.ACTIVITY}</td>
+              <td>${act.VENUE}</td>
+              <td>${act.PROPONENT}</td>
+              <td>${act.ATTENDEES}</td>
+            </tr>`;
+    });
+
+    document.getElementById('modal').style.display = 'flex';
+  }
+
+  function closeModal() {
+    document.getElementById('modal').style.display = 'none';
+  }
